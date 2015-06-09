3b1df098c8f4c4e3217ae89eaa5303517a72e9ef
Peer Rails <peer@omck.ws>
2015-06-09 11:46:05 +0300

    god left me unfinished

--- job_model.rb
+++ job_model.rb
@@ -19,7 +19,7 @@ class Jobs < DB
     self.where(done: false).get.each do |job|
       update_job  = Jobs.new
       download_path = download_file(job["img_url"], "/var/www/files")
-      update_job.update(job["id"], downloaded: "\'#{download_path}\'" ) if job["downloaded"].nil? || job["downloaded"] == '' || download_path.nil? || download_path != ''
+      update_job.update(job["id"], downloaded: "\'#{download_path}\'" ) if job["downloaded"].nil? || job["downloaded"] == '' || !download_path.nil? || download_path != ''
     end
   end
 
